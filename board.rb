8db68177081527d4b5fe5ff936f9ddeea1a30e80
matristle <mercerary@tutanota.com>
2022-05-19 21:13:13 +0300

    Make #make_grid_with_nodes and #append_adjacent_node_positions

--- board.rb
+++ board.rb
@@ -1,16 +1,49 @@
-class Board
-  attr_accessor :board_nested_arrays
-  def initialize
-    @board_nested_arrays = Array.new(8) { Array.new(8, '') }
-    8.times do |row_index|
-      make_row(@board_nested_arrays, row_index)
-    end
-    make_zipping_line
+require_relative 'node'
+require_relative 'linked_list'
 
-    pp @board_nested_arrays
+class Board
+  attr_accessor :board_grid
+  
+  def initialize
+    make_grid_with_nodes
+    append_adjacent_node_positions
   end
 
-  def make_row(board_nested_arrays, row_index)
+  def make_grid_with_nodes
+    @board_grid = Array.new(8) { Array.new(8) }
+
+    8.times do |row_index|
+      8.times do |column_index|
+        @board_grid[row_index][column_index] = Node.new
+      end
+    end
+
+    return board_grid
+  end
+
+
+  def append_adjacent_node_positions
+    8.times do |row_index|
+      8.times do |column_index|
+        current_node = board_grid[row_index][column_index]
+        
+        current_node.adjacent_node_positions << [row_index, column_index + 1] unless column_index + 1 > 7
+        current_node.adjacent_node_positions << [row_index, column_index - 1] unless column_index - 1 < 0
+
+        current_node.adjacent_node_positions << [row_index + 1, column_index] unless row_index + 1 > 7
+        current_node.adjacent_node_positions << [row_index - 1, column_index] unless row_index - 1 < 0
+      end
+    end
+  end
+
+  def print_board(board_grid)
+    8.times do |row_index|
+      make_row(board_grid, row_index)
+    end
+    make_zipping_line
+  end
+ 
+  def make_row(board_grid, row_index)
     (8 + 1).times do 
       print '----'
     end
@@ -18,7 +51,7 @@ class Board
     print '|'
     (8 + 1).times do |column_index|
       print '   |'
-      board_nested_arrays[row_index][column_index] = '   |'
+      board_grid[row_index][column_index] = '   |'
     end
     print "\n"
   end
@@ -31,7 +64,9 @@ class Board
   end
 end
 
-board = Board.new
+
+
+
 
 
 
