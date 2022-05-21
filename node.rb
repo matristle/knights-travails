75b830c196336606d526ca890fba01883bf4f737
matristle <mercerary@tutanota.com>
2022-05-21 13:24:11 +0300

    Add parent attribute

--- node.rb
+++ node.rb
@@ -1,9 +1,10 @@
 require_relative 'board'
 
 class Node
-  attr_accessor :adjacent_node_positions
+  attr_accessor :adjacent_node_positions, :parent
 
   def initialize
     @adjacent_node_positions = []
+    @parent = nil
   end
 end
