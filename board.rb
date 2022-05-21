d5ee63cbee7ab4dd539ba1669165f3302e111eeb
matristle <mercerary@tutanota.com>
2022-05-21 17:55:33 +0300

    Make #make_chess_coordinates

--- board.rb
+++ board.rb
@@ -1,12 +1,27 @@
 require_relative 'node'
-require_relative 'linked_list'
 
 class Board
   attr_accessor :board_grid
   
   def initialize
     @board_grid = Array.new(8) { Array.new(8) { Node.new } }
+    make_chess_coordinates
     append_adjacent_node_positions
+  end
+
+  def make_chess_coordinates
+    column_letter_array = ('A'..'H').to_a
+    row_number_array = (1..8).to_a.reverse
+
+    8.times do |row_index|
+      8.times do |column_index|
+        current_node = board_grid[row_index][column_index]
+
+        current_node.chess_coordinates = "#{column_letter_array[column_index]}#{row_number_array[row_index]}"
+      end
+    end
+
+
   end
 
   def append_adjacent_node_positions
