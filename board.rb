93958bd20872ad97dabf2253d8e61e672db552bc
matristle <mercerary@tutanota.com>
2022-05-21 13:28:51 +0300

    Delete unnecessary methods for printing the board

--- board.rb
+++ board.rb
@@ -5,68 +5,35 @@ class Board
   attr_accessor :board_grid
   
   def initialize
-    make_grid_with_nodes
+    @board_grid = Array.new(8) { Array.new(8) { Node.new } }
     append_adjacent_node_positions
   end
-
-  def make_grid_with_nodes
-    @board_grid = Array.new(8) { Array.new(8) }
-
-    8.times do |row_index|
-      8.times do |column_index|
-        @board_grid[row_index][column_index] = Node.new
-      end
-    end
-
-    return board_grid
-  end
-
 
   def append_adjacent_node_positions
     8.times do |row_index|
       8.times do |column_index|
         current_node = board_grid[row_index][column_index]
-        
-        current_node.adjacent_node_positions << [row_index, column_index + 1] unless column_index + 1 > 7
-        current_node.adjacent_node_positions << [row_index, column_index - 1] unless column_index - 1 < 0
 
-        current_node.adjacent_node_positions << [row_index + 1, column_index] unless row_index + 1 > 7
-        current_node.adjacent_node_positions << [row_index - 1, column_index] unless row_index - 1 < 0
+        current_node.adjacent_node_positions << [row_index - 2, column_index - 1] unless row_index - 2 < 0 || column_index - 1 < 0
+        current_node.adjacent_node_positions << [row_index - 2, column_index + 1] unless row_index - 2 < 0 || column_index + 1 > 7
+
+        current_node.adjacent_node_positions << [row_index - 1, column_index - 2] unless row_index - 1 < 0 || column_index - 2 < 0
+        current_node.adjacent_node_positions << [row_index - 1, column_index + 2] unless row_index - 1 < 0 || column_index + 2 > 7
+
+        current_node.adjacent_node_positions << [row_index + 1, column_index - 2] unless row_index + 1 > 7 || column_index - 2 < 0
+        current_node.adjacent_node_positions << [row_index + 1, column_index + 2] unless row_index + 1 > 7 || column_index + 2 > 7
+
+        current_node.adjacent_node_positions << [row_index + 2, column_index - 1] unless row_index + 2 > 7 || column_index - 1 < 0
+        current_node.adjacent_node_positions << [row_index + 2, column_index + 1] unless row_index + 2 > 7 || column_index + 1 > 7
+        
+        # current_node.adjacent_node_positions << [row_index, column_index + 1] unless column_index + 1 > 7
+        # current_node.adjacent_node_positions << [row_index, column_index - 1] unless column_index - 1 < 0
+
+        # current_node.adjacent_node_positions << [row_index + 1, column_index] unless row_index + 1 > 7
+        # current_node.adjacent_node_positions << [row_index - 1, column_index] unless row_index - 1 < 0
       end
     end
   end
-
-  def print_board(board_grid)
-    8.times do |row_index|
-      make_row(board_grid, row_index)
-    end
-    make_zipping_line
-  end
- 
-  def make_row(board_grid, row_index)
-    (8 + 1).times do 
-      print '----'
-    end
-    print "\n"
-    print '|'
-    (8 + 1).times do |column_index|
-      print '   |'
-      board_grid[row_index][column_index] = '   |'
-    end
-    print "\n"
-  end
-
-  def make_zipping_line
-    (8 + 1).times do 
-      print '----'
-    end
-    print "\n"
-  end
-end
-
-
-
-
 
 
 
