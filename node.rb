1528a946c08a09acd4c29458e9eb5c788b492d01
matristle <mercerary@tutanota.com>
2022-05-21 18:09:15 +0300

    Add chess_coordinates attribute

--- node.rb
+++ node.rb
@@ -1,10 +1,11 @@
 require_relative 'board'
 
 class Node
-  attr_accessor :adjacent_node_positions, :parent
+  attr_accessor :adjacent_node_positions, :parent, :chess_coordinates
 
   def initialize
     @adjacent_node_positions = []
     @parent = nil
+    @chess_coordinates = nil
   end
 end
